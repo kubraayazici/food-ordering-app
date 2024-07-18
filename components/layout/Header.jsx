da38397ff8263dd84f7c94d099fc89a948f3d851
Sunbul Kubra Yazici <sunbul@sunbul-air.local>
2024-07-18 22:16:02 +0300

    Outsideclick handler is used. Installed with npm.

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import Logo from "../ui/Logo";
-import { FaUser, FaShoppingCart, FaSearch  } from "react-icons/fa";
+import Title from "../ui/Title";
+import { FaUser, FaShoppingCart, FaSearch, FaLessThanEqual } from "react-icons/fa";
+import OutsideClickHandler from "react-outside-click-handler";
 
 const Header = () => {
+  const [isSearchModal,setIsSearchModal] = useState(false);
+  const handleClick = () => {
+    setIsSearchModal(true);
+  };
+  const handleOutsideclick = () => {
+    setIsSearchModal(false);
+  };
   return (
-    <div className="h-[5.5rem] bg-secondary ">
+      <div className="h-[5.5rem] bg-secondary ">
       <div className="container text-white mx-auto flex justify-between items-center h-full">
-        <div>
-              <Logo/>
-        </div>
-        
+        <div><Logo/></div>
         <nav>
           <ul className="flex gap-x-2">
             <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer"><a href="">Home</a></li>
@@ -17,15 +24,24 @@ const Header = () => {
             <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer"><a href="">Book Table</a></li>
           </ul>
         </nav>
-        
         <div className="flex gap-x-3 items-center">
-          <a href="#"><FaUser/></a>
-          <a href="#"><FaShoppingCart/></a>
-          <a href="#"><FaSearch/></a>
+          <a href="#"><FaUser className="hover:text-primary transition-all"/></a>
+          <a href="#"><FaShoppingCart className="hover:text-primary transition-all"/></a>
+          <button onClick={handleClick}><FaSearch className="hover:text-primary transition-all"/></button>
           <a href="#"><button className="btn-primary">Order Online</button></a>
+          
         </div>
+        
       </div>
+      {isSearchModal && (
+        <OutsideClickHandler onOutsideClick={handleOutsideclick}>
+          <div className="modal">
+            <Title addClass="text-9xl">Search</Title>
+          </div>
+        </OutsideClickHandler>
+      )}
     </div>
+    
   );
 };
 
