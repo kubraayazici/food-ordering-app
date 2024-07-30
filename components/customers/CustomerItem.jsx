22d695439268224e93f109c7cdfbca7d363a8fae
Sunbul Kubra Yazici <sunbul@192.168.1.7>
2024-07-30 15:06:27 +0300

    Customers component added.

---
+++ components/customers/CustomerItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Image from "next/image"
+
+const CustomerItem = ({imgSrc}) => {
+  return (
+    <div className='mt-5'>
+        <div className='p-6 bg-secondary text-white rounded-md'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium consectetur, numquam neque qui vel officia aliquid excepturi? Dolorum sint qui possimus nostrum nobis unde blanditiis, numquam ab pariatur ducimus voluptas.</p>
+            <div className='flex flex-col mt-4'>
+                <span className='text-lg font-semibold'>Moana Michell</span>
+                <span className='text-sm'>magna aliqua</span>
+            </div>
+        </div>
+
+        <div className="relative w-28 h-28 border-4 border-primary rounded-full mt-8 before:content-[''] before:absolute before:top-0 flex justify-center before:-translate-y-3 before:rotate-45 before:bg-primary before:w-5 before:h-5">
+            <Image src={imgSrc} alt='' layout='fill' objectFit='contain' className='rounded-full'></Image>
+        </div>
+    </div>
+  )
+}
+
+export default CustomerItem
--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -4,18 +4,19 @@ import Campaigns from '../../components/Campaigns'
 import MenuWrapper from '../../components/product/MenuWrapper'
 import About from '../../components/About'
 import Reservation from '../../components/Reservation'
+import Customers from '../../components/customers/Customers'
 const Home = () => {
   return (
-    // <React.Fragment>
-    <div>
+    <React.Fragment>
+    
       <Carousel/>
       <Campaigns/>
       <MenuWrapper/>
       <About/>
       <Reservation/>
-    </div>
+      <Customers/>
       
-    // </React.Fragment>
+    </React.Fragment>
   )
 }
 
